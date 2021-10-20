1ccf5a3270d0c64e4b0a1831d111effcc0244bf9
Chang hyun Jang <cjang@c8r7s8.42seoul.kr>
2021-10-20 14:57:43 +0900

    server_제출 version

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Dockerfile                                         :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/03/29 18:59:33 by cjang             #+#    #+#              #
+#    Updated: 2021/04/02 16:08:06 by cjang            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# install debian
+FROM	debian:buster
+
+# my info
+LABEL	maintainer="cjang@student.42seoul.kr"
+LABEL	title="ft_server"
+LABEL	version="1.0"
+
+# install applications
+RUN		apt-get -y update
+RUN		apt-get -y install nginx
+RUN		apt-get -y install php7.3-fpm
+RUN		apt-get -y install php-mysql
+RUN		apt-get -y install mariadb-server
+RUN		apt-get -y install openssl
+RUN		apt-get -y install wget
+
+# copy settings
+COPY	srcs/default /cjang_srcs/
+COPY	srcs/phpinfo.php /cjang_srcs/
+COPY	srcs/wp-config.php /cjang_srcs/
+COPY	srcs/config.inc.php /cjang_srcs/
+COPY	srcs/init_server.sh ./
+COPY	srcs/cmd_server.sh ./
+
+# port number
+EXPOSE	80
+EXPOSE	443
+
+# install phpmyadmin, wordpress / copy settings / add mysql id, db / add ssl
+RUN		bash init_server.sh
+
+# execute bash
+CMD		bash cmd_server.sh
---
+++ srcs/init_server.sh
@@ -0,0 +1,46 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    init_server.sh                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cjang <cjang@student.42seoul.kr>           +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/03/31 17:01:45 by cjang             #+#    #+#              #
+#    Updated: 2021/04/02 16:56:41 by cjang            ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# install phpmyadmin
+wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
+tar -xvf phpMyAdmin-5.0.2-all-languages.tar.gz
+mv phpMyAdmin-5.0.2-all-languages phpmyadmin
+mv phpmyadmin /var/www/html/
+rm -rf phpMyAdmin-5.0.2-all-languages.tar.gz
+
+# install wordpress
+wget https://wordpress.org/latest.tar.gz
+tar -xvf latest.tar.gz
+mv wordpress/ var/www/html/
+rm -rf latest.tar.gz
+
+# change wordpress owner
+chown -R www-data:www-data /var/www/html/wordpress
+
+# copy settings
+cp /cjang_srcs/default /etc/nginx/sites-available/
+cp /cjang_srcs/phpinfo.php /var/www/html/
+cp /cjang_srcs/wp-config.php /var/www/html/wordpress
+cp /cjang_srcs/config.inc.php /var/www/html/phpmyadmin/
+
+# add mysql user, database
+service mysql start
+mysql -e "CREATE USER 'cjang'@'%' identified by '888';"
+mysql -e "CREATE DATABASE wordpress;"
+mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'cjang'@'%' identified by '888';"
+
+# add private key, public key, csr, crt
+openssl genrsa -out /etc/ssl/private/cjang_private.key
+openssl rsa -in /etc/ssl/private/cjang_private.key -pubout -out /etc/ssl/private/cjang_public.key
+openssl req -subj "/C=KR/ST=Seoul/L=GangNam/O=42SEOUL/CN=cjang" -new -key /etc/ssl/private/cjang_private.key -out /etc/ssl/private/cjang_csr.csr
+openssl x509 -req -days 365 -signkey /etc/ssl/private/cjang_private.key -in /etc/ssl/private/cjang_csr.csr -out /etc/ssl/certs/cjang_crt.crt
+chmod 600 /etc/ssl/private/cjang_private.key
